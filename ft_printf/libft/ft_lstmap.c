a6f536f2bca6be9296a8ed58903c70b36b7dd6b9
Andre Alexandre Da Silva Piedade De Albuquerque <andre-da@c1r16s3.42lisboa.com>
2023-10-26 14:57:39 +0100

    update

--- ft_printf/libft/ft_lstmap.c
+++ ft_printf/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: andre-da <andre-da@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 15:29:07 by andre-da          #+#    #+#             */
-/*   Updated: 2023/10/21 16:50:29 by andre-da         ###   ########.fr       */
+/*   Updated: 2023/10/26 14:47:32 by andre-da         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
+	t_list	*process;
 
 	if (!lst || !f)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		process = f(lst->content);
+		new_node = ft_lstnew(process);
 		if (new_node == NULL)
 		{
+			del(process);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
