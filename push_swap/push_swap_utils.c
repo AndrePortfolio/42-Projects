9038ac813ced9cfa1a3103a7f0cbe265707f5aeb
Andre Albuquerque <andrealbuquerque@iMac.home>
2023-12-08 14:56:39 +0000

    update

--- push_swap/push_swap_utils.c
+++ push_swap/push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: andrealbuquerque <andrealbuquerque@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 11:08:22 by andre-da          #+#    #+#             */
-/*   Updated: 2023/12/07 22:40:31 by andrealbuqu      ###   ########.fr       */
+/*   Updated: 2023/12/08 14:34:56 by andrealbuqu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,32 @@ t_stack *find_last(t_stack *stack)
 	while (stack->next)
 		stack = stack->next;
 	return (stack);
+}
+
+int	stack_len(t_stack *stack)
+{
+	int	lenght;
+
+	lenght = 0;
+	if (!stack)
+		return (0);
+	while (stack)
+	{
+		stack = stack->next;
+		lenght++;
+	}
+	return (lenght);
+}
+
+bool	is_sorted(t_stack *stack)
+{
+	if (!stack)
+		return (false);
+	while (stack->next)
+	{
+		if (stack->nbr > stack->next->nbr)
+			return (false);
+		stack = stack->next;
+	}
+	return (true);
 }
