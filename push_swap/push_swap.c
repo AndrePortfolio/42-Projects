272e2f6fe0d0d0cfbdb6ff2315cf9ec7f307fe40
Andre Albuquerque <andrealbuquerque@iMac.home>
2024-02-03 09:44:51 +0000

    done

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: andrealbuquerque <andrealbuquerque@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 13:24:01 by andre-da          #+#    #+#             */
-/*   Updated: 2024/01/19 12:17:06 by andrealbuqu      ###   ########.fr       */
+/*   Updated: 2024/02/03 09:31:38 by andrealbuqu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		return (0);
 	else if (argc == 2 && !argv[1][0])
+	{
 		write(2, "Error\n", 6);
+		return (1);
+	}
 	else if (argc == 2)
 	{
 		argv = ft_split(argv[1], ' ');
