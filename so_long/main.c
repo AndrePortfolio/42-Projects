c62a54725c82e6eb010d8aad15cf55c32be207e9
Andre Alexandre Da Silva Piedade De Albuquerque <andre-da@c2r7s3.42lisboa.com>
2024-02-05 20:17:37 +0000

    update

--- so_long/main.c
+++ so_long/main.c
@@ -18,7 +18,11 @@ typedef struct	s_vars
 int	close_win(int keycode, t_vars *vars)
 {
 	if (keycode == ESC)
+	{
 		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_display(vars->mlx);
+		exit (0);
+	}
 	return (0);
 }
 
@@ -45,22 +49,24 @@ int	main(void)
 {
 	t_data	img;
 	t_vars	vars;
+	int width = 800;
+	int	height = 500;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	vars.win = mlx_new_window(vars.mlx, width, height, "Hello world!");
 	mlx_hook(vars.win, 2, 1L<<0, close_win, &vars);
 	mlx_hook(vars.win, 12, 1L<<15, handle_resize, &vars);
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
+	img.img = mlx_new_image(vars.mlx, width, height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
 	int	white = 0x00FFFFFF;
 	int red = 0x00FF0000;
-	int	x = 0;
-	int	y = 0;
+	int	x = 100;
+	int	y = 100;
 
-	while (x < 1920)
+	while (x < 200)
 	{
-		while (y < 1080)
+		while (y < 200)
 		{
 			int color = create_trgb(0, 0, 0, 100);
 			my_mlx_pixel_put(&img, x, y, color);
