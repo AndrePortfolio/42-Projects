ba9da1118177aad4a09287f67912a24533d2574a
Andre Albuquerque <andrealbuquerque@iMac.home>
2024-03-15 01:04:26 +0000

    finished

---
+++ philo/sources/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrealbuquerque <andrealbuquerque@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 00:22:37 by andrealbuqu       #+#    #+#             */
+/*   Updated: 2024/03/15 01:02:58 by andrealbuqu      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
